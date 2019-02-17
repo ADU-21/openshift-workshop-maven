e780a17b05eb7374885da47200f41c54058fbe3b
yidong.dyd <yidong.dyd@alibaba-inc.com>
2019-02-17 15:10:45 +0800

    [Yidong]init spring boot applicationi

---
+++ src/main/java/adu/openshift/maven/spring/boot/workshop/WorkshopApplication.java
@@ -0,0 +1,16 @@
+package adu.openshift.maven.spring.boot.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yidong
+ */
+@SpringBootApplication
+public class WorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopApplication.class, args);
+    }
+
+}
